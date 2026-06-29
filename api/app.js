62ce2843daeb7ca4ace38029db036bdb64fc2a23
ZUMER115 <157277943+ZUMER115@users.noreply.github.com>
2026-06-29 04:23:02 -0700

    Added CORS and thicker test suite

--- api/app.js
+++ api/app.js
@@ -1,9 +1,17 @@
 // Import the necessary modules and create an instance of the Express application to act as the server for the API
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 const app = express();
 
 // Import the dotenv package to manage env variables
 require('dotenv').config();
+
+// Set secure HTTP headers
+app.use(helmet());
+
+// Restrict cross-origin requests
+app.use(cors({ origin: false }));
 
 // Allow the app to read JSON data from the request body
 app.use(express.json());
