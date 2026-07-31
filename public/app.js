ebaa11c504e0eff27738257c2faa33e30af673bc
ZUMER115 <157277943+ZUMER115@users.noreply.github.com>
2026-07-31 05:17:52 -0700

    Frontend: Added Register and Login elements

--- public/app.js
+++ public/app.js
@@ -3,12 +3,40 @@ function myFunction() {
 }
 
 async function fetchData() {
-    const response = await fetch('http://107.23.220.85:3067/api/characters');
+    const response = await fetch('http://107.23.220.85:3067/api/auth/register');
     if (!response.ok) {
         document.getElementById("characters").innerHTML = "Error fetching data: " + response.statusText;
         return;
     } else {
         const data = await response.json();
         document.getElementById("characters").innerHTML = JSON.stringify(data);
+    }
+}
+
+async function registerUser() {
+    const username = document.getElementById("regUsername").innerHTML.value;
+    const password = document.getElementById("regPassword").innerHTML.value;
+
+    const response = await fetch('http://107.23.220.85:3067/api/characters');
+    if (!response.ok) {
+        document.getElementById("register").innerHTML = "Error registering user: " + response.statusText;
+        return;
+    } else {
+        const data = await response.json();
+        document.getElementById("register").innerHTML = JSON.stringify(data);
+    }
+}
+
+async function loginUser() {
+    const username = document.getElementById("logUsername").innerHTML.value;
+    const password = document.getElementById("logPassword").innerHTML.value;
+
+    const response = await fetch('http://107.23.220.85:3067/api/auth/login');
+    if (!response.ok) {
+        document.getElementById("login").innerHTML = "ERROR loggin in user: " + response.statusText;
+        return;
+    } else {
+        const data = await response.json();
+        document.getElementById("login").innerHTML = JSON.stringify(data);
     }
 }
